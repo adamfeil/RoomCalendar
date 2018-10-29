d1e269fd0573997a626ee14046ba67cb99ad08c4
Adam Feil <adam@makestickers.com>
2018-10-29 16:14:36 -0500

    Added Code

---
+++ RoomCalendars/Controllers/CalDataController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Ical.Net;
+using Ical.Net.CalendarComponents;
+
+namespace RoomCalendars.Controllers
+
+
+
+
+
+{
+
+    public class EventDTO
+    {
+        public DateTime begin { get; set; }
+        public DateTime end { get; set; }
+        public string summary { get; set; }
+        public string organizer { get; set; }
+    }
+
+
+
+
+
+    public class CalDataController : ApiController
+    {
+
+        public List<EventDTO> getEvents(string calendarURL)
+        {
+
+            var client = new WebClient();
+            var calData = client.DownloadString(calendarURL);
+
+            var c = Calendar.Load(calData);
+
+            var occ = c.GetOccurrences(DateTime.Now, DateTime.Now.AddDays(7)).OrderBy(r => r.Period.StartTime.Value.ToUniversalTime()).ToList();
+
+            occ = occ.Where(o => o.Period.EndTime.Value.ToUniversalTime() > DateTime.Now.ToUniversalTime()).ToList();
+
+             var returnEvents = new List<EventDTO>();
+
+            foreach (var o in occ)
+            {
+                if (o.Source.GetType().Name == "CalendarEvent")
+                {
+                    var e = new EventDTO();
+                    e.begin = o.Period.StartTime.Value;
+                    e.end = o.Period.EndTime.Value;
+                    var x = (CalendarEvent)o.Source;
+                    if(x.Organizer != null)
+                    {
+                        e.organizer = x.Organizer.CommonName;
+                    }
+                    else
+                    {
+                        e.organizer = "";
+                    }
+                    
+                    e.summary = x.Summary;
+
+                    returnEvents.Add(e);
+                    
+                }
+               
+                
+
+            }
+
+            
+
+           
+          
+            
+            return returnEvents;
+
+
+        }
+
+    }
+}
---
+++ RoomCalendars/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Ical.Net;
+
+namespace RoomCalendars.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            var client = new WebClient();
+            var calData = client.DownloadString("https://calendar.google.com/calendar/ical/graphicsland.com_3830353733323030333330%40resource.calendar.google.com/private-33cf26ee7eca3c690862dc30e808a12d/basic.ics");
+
+           var c =  Calendar.Load(calData);
+
+            var events = c.Events.ToList();
+
+
+
+            return View(events);
+        }
+    }
+}
